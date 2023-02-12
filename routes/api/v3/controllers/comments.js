4f5c214643fa5255b5d0dba46fee4cfc773641bc
freymond <37636251+freymondz@users.noreply.github.com>
2023-02-11 21:36:51 -0800

    finish a6

--- routes/api/v3/controllers/comments.js
+++ routes/api/v3/controllers/comments.js
@@ -2,12 +2,39 @@ import express from 'express';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-
+router.get('/', async (req, res) => {
+    try {
+        const postID = req.query.postID;
+        const post = await req.models.comment.find({ post: postID });
+        res.status(200)
+            .json(post);
+    } catch (error) {
+        res.status(500)
+            .json({ error });
+    }
 });
 
 router.post('/', (req, res) => {
-    
+    if (!req.session.isAuthenticated) {
+        res.status(401)
+            .json({ status: 'error', error: 'not logged in' });
+        return;
+    }
+
+    try {
+        const comment = {
+            username: req.session.account.username,
+            comment: req.body.newComment,
+            post: req.body.postID,
+            created_date: new Date(),
+        };
+        req.models.comment.create(comment);
+
+        res.status(200).json({ status: 'success' });
+    } catch (error) {
+        res.status(500)
+            .json({ error });
+    }
 });
 
 export default router;
--- routes/api/v3/controllers/posts.js
+++ routes/api/v3/controllers/posts.js
@@ -4,16 +4,69 @@ const router = express.Router();
 
 import getURLPreview from '../utils/urlPreviews.js';
 
-router.post('/like', (req, res) => {
-
+router.post('/like', async (req, res) => {
+    if (!req.session.isAuthenticated) {
+        res.status(401)
+            .json({ status: 'error', error: 'not logged in' });
+        return;
+    }
+    try {
+        const post = await req.models.post.findOne({ _id: req.body.postID });
+        if (!post.likes.includes(req.session.account.username)) {
+            post.likes.push(req.session.account.username);
+        }
+        await post.save();
+        res.status(200).json({ status: 'success' });
+    } catch (error) {
+        console.log(error)
+        res.status(500)
+            .json({ error });
+    }
 });
 
-router.post('/unlike', (req, res) => {
+router.post('/unlike', async (req, res) => {
+    if (!req.session.isAuthenticated) {
+        res.status(401)
+            .json({ status: 'error', error: 'not logged in' });
+        return;
+    }
 
+    try {
+        const post = await req.models.post.findOne({ _id: req.body.postID });
+        if (post.likes?.includes(req.session.account.username)) {
+            post.likes.splice(post.likes.indexOf(req.session.account.username), 1);
+        }
+        await post.save();
+        res.status(200).json({ status: 'success' });
+    } catch (error) {
+        console.log(error)
+        res.status(500)
+            .json({ error });
+    }
 });
 
-router.delete('/', (req, res) => {
-    
+router.delete('/', async (req, res) => {
+    if (!req.session.isAuthenticated) {
+        res.status(401)
+            .json({ status: 'error', error: 'not logged in' });
+        return;
+    }
+
+    try {
+        const post = await req.models.post.findOne({ _id: req.body.postID });
+        if (post.username !== req.session.account.username) {
+            res.status(401).json({ status: 'error', error: 'you can only delete your own posts' });
+            return;
+        }
+        
+        await req.models.comment.deleteMany({ post: req.body.postID });
+        await req.models.post.deleteOne({ _id: req.body.postID });
+        res.status(200).json({ status: 'success' });
+    } catch (error) {
+        console.log(error)
+        res.status(500)
+            .json({ error });
+    }
 });
 
 router.post('/', (req, res) => {
@@ -28,6 +81,7 @@ router.post('/', (req, res) => {
             ...req.body,
             username: req.session.account.username,
             created_date: new Date(),
+            likes: [],
         };
         req.models.post.create(post);
 
@@ -52,9 +106,12 @@ router.get('/', async (req, res) => {
                 try {
                     const preview = await getURLPreview(post.url);
                     return {
+                        id: post._id,
                         description: post.description,
                         username: post.username,
                         htmlPreview: preview,
+                        created_date: post.created_date,
+                        likes: post.likes,
                     };
                 } catch (error) {
                     return {
